2261551dbcc8e790600836042cd6a1fe7831e96f
Stef Vercaemer <stefaan.vercaemer@howest.be>
2019-10-06 16:00:20 +0200

    Add files via upload

---
+++ Vrienden.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Vrienden.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ToonHuidigeVrienden();
+        }
+
+        void ToonHuidigeVrienden()
+        {
+
+        }
+
+        void VulListBox()
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            VulListBox();
+        }
+    }
+}
